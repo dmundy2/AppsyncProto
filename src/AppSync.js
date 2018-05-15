0b57ecce2da94bbec6a92d1eb94e941934b03d88
Dylan Mundy <v-dylanmundy@smarttech.com>
2018-05-15 09:13:59 +0200

    update API key

--- src/AppSync.js
+++ src/AppSync.js
@@ -2,5 +2,5 @@ export default {
   'graphqlEndpoint': 'https://bl7yl3zgkbdc7c52t5wtjnyb2a.appsync-api.us-east-2.amazonaws.com/graphql',
   'region': 'us-east-2',
   'authenticationType': 'API_KEY',
-  'apiKey': 'da2-nkmq27jxofd67ajtavayzidi2e'
+  'apiKey': 'da2-5r7xxaeltze4lac3f5rskrv244'
 };
